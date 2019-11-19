2aa06cb3888618372b3551f36739c9495e60ee2f
m1hailp3tkov <41004048+m1hailp3tkov@users.noreply.github.com>
2019-11-19 03:22:34 +0200

    added students problem

--- hw_6/STUDENTS/app.js
+++ hw_6/STUDENTS/app.js
@@ -1,1 +1,64 @@
-console.log('TODO...');
+const username = 'mihail';
+const password = '123456';
+const url = 'https://baas.kinvey.com/appdata/kid_SJRQa2ehB/students';
+
+const tBody = document.querySelector("#results > tbody");
+
+function generateHeaders(type, body)
+{
+    let obj = 
+    {
+        method: type,
+        headers:
+        {
+            'Authorization': `Basic ${btoa(`${username}:${password}`)}`
+        }
+    }
+
+    if(type !== 'GET') obj.headers['Content-Type'] = 'application/json';
+    if(body) obj.body = body;
+
+    return obj;
+}
+
+async function displayStudents()
+{
+    let students;
+
+    await fetch(url, generateHeaders('GET'))
+    .then(x => x.json())
+    .then(x => students = x);
+
+    students.sort((a,b) => a.ID - b.ID);
+
+    for(let s of students)
+    {
+        let tr = document.createElement('tr');
+        
+        let id = createTd();
+        id.textContent = s.ID;
+        let firstName = createTd();
+        firstName.textContent = s.FirstName;
+        let lastName = createTd();
+        lastName.textContent = s.LastName;
+        let facNum = createTd();
+        facNum.textContent = s.FacultyNumber;
+        let grade = createTd();
+        grade.textContent = s.Grade;
+
+        tr.appendChild(id);
+        tr.appendChild(firstName);
+        tr.appendChild(lastName);
+        tr.appendChild(facNum);
+        tr.appendChild(grade);
+
+        tBody.appendChild(tr);
+    }
+}
+
+function createTd()
+{
+    return document.createElement('td');
+}
+
+displayStudents();
