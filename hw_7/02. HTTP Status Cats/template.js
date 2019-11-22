1d5bdbc3414c86717e8e5cd2e41ef7c15e9b19b5
m1hailp3tkov <41004048+m1hailp3tkov@users.noreply.github.com>
2019-11-22 20:15:45 +0200

    added prob 2

--- hw_7/02. HTTP Status Cats/template.js
+++ hw_7/02. HTTP Status Cats/template.js
@@ -1,8 +1,32 @@
-(() => {
-     renderCatTemplate();
+(async () => {
+     await renderCatTemplate();
+     attachButtonEventHandlers();
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
+     async function renderCatTemplate() 
+     {
+         const catsSection = document.getElementById('allCats');
+         const source = await fetch('http://127.0.0.1:5500/02.%20HTTP%20Status%20Cats/cats.hbs')
+         .then(x => x.text());
+
+         const template = Handlebars.compile(source);
+         const catsHtml = template({cats: window.cats});
+
+         catsSection.innerHTML = catsHtml;
+     }
+
+     function attachButtonEventHandlers()
+     {
+         const buttons = document.getElementsByClassName('showBtn');
+
+         for(let button of buttons)
+         {
+             button.addEventListener('click', e => showInfo(e.target));
+         }
+     }
+
+     function showInfo(target)
+     {
+        target.parentNode.childNodes[3].style.display = 'block';
      }
  
-})
+})();
