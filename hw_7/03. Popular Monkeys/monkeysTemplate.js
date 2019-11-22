67f33b012921c6a1974f98f3bd386efbdd9813ca
m1hailp3tkov <41004048+m1hailp3tkov@users.noreply.github.com>
2019-11-22 20:57:16 +0200

    added prob 3

--- hw_7/03. Popular Monkeys/monkeysTemplate.js
+++ hw_7/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,20 @@
-$(() => {
-    // TODO
-})
+(async () => 
+{
+    let source = await fetch('http://127.0.0.1:5500/03.%20Popular%20Monkeys/monkeys.hbs')
+    .then(x => x.text());
+
+    const monkeysDiv = document.querySelector("body > section > div");
+    const buttons = document.getElementsByTagName('button');
+
+    let template = Handlebars.compile(source);
+
+    monkeysDiv.innerHTML = template({monkeys: window.monkeys});
+
+    for(let button of buttons)
+    {
+        button.addEventListener('click', e =>
+        {
+            e.target.parentNode.querySelector('p').style.display = 'block';
+        });
+    }
+})();
